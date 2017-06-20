e9d863a30f89ecf0418039696daab4941b23af98
Bridger <bridgerrhammond@gmail.com>
2017-06-20 16:09:47 -0400

    Grunt doesn't work with Foundation. Something to do with the selectors Foundation uses.

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('default', function() {
+});
+
+// Concat
+gulp.task('concatjs', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('concat.js'))
+    .pipe(gulp.dest('src/js/'));
+});
+gulp.task('concatcss', function() {
+  return gulp.src('src/css/*.css')
+    .pipe(concat('concat.css'))
+    .pipe(gulp.dest('src/css/'));
+});
+
+// Minify
+gulp.task('minifyjs', function (cb) {
+  pump([
+        gulp.src('src/js/concat.js'),
+        uglify(),
+        gulp.dest('app/js/')
+    ],
+    cb
+  );
+});
+gulp.task('minifycss', function() {
+  return gulp.src('src/css/concat.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('app/css/'));
+});
