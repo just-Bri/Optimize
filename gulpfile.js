2d5e1a5382733255cfb66ce89f023f9d4749fd97
Bridger <bridgerrhammond@gmail.com>
2017-06-21 15:24:35 -0400

    Why is jquery requesting fonts? Chrome Dev Tools bug?

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('concatjs', function() {
       'src/js/fastclick.js'
     ], { base: './' }))
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('app/js/'));
+    .pipe(gulp.dest('src/js/'));
 });
 gulp.task('concatcss', function() {
   return gulp.src('src/css/*.css')
