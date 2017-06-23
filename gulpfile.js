9d5f3efed29296427168702ef5df9c0f5f2b86da
Bridger <bridgerrhammond@gmail.com>
2017-06-23 14:16:55 -0400

    Sprite sheet issues

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,9 @@ var image = require('gulp-image');
 var pngquant = require('imagemin-pngquant');
 var jpgoptim = require('imagemin-jpegoptim');
 var clean = require('gulp-clean');
-var spritesmith = require('gulp.spritesmith');
 
 // Smash Js
-gulp.task('smashJs', function (cb) {
+gulp.task('smashjs', function (cb) {
   pump([
         gulp.src(['src/js/jquery.js', 'src/js/foundation.js', 'src/js/foundation.equalizer.js', 'src/js/foundation.reveal.js','src/js/fastclick.js', 'src/js/scripts.js']),
         concat('main.js'),
@@ -33,7 +32,7 @@ gulp.task('smashJs', function (cb) {
 });
 
 // Smash Css
-gulp.task('smashCss', function() {
+gulp.task('smashcss', function() {
   return gulp.src('src/css/*.css')
     .pipe(concat('main.css'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
@@ -62,20 +61,8 @@ gulp.task('smashimg', function () {
       svgo: true,
       concurrent: 10
     }))
-    .pipe(gulp.dest('app/img/'));
+    .pipe(gulp.dest('src/img/'));
 });
 
 // Build
-gulp.task('default', gulpsequence('smashhtml', 'smashJs', 'smashCss', 'smashimg'));
-
-// New sprite stuff
-gulp.task('sprite', function () {
-    var spriteData = gulp.src('img/avatars/*.png')
-        .pipe(spritesmith({
-            /* this whole image path is used in css background declarations */
-            imgName: '../assets/img/sprite.png',
-            cssName: 'sprite.css'
-        }));
-    spriteData.img.pipe(gulp.dest('img'));
-    spriteData.css.pipe(gulp.dest('css'));
-});
+gulp.task('default', gulpsequence('smashhtml', 'smashjs', 'smashcss', 'smashimg'));
